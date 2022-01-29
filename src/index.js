0770324bbc7d2f10c876cfac8f7a6b52fc1af8a2
vitaliy-1111 <networkvetalius@gmail.com>
2022-01-29 19:05:54 +0200

    add fetch

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const homePage = `<form class="search-form">
       again
     </p>`;
 const libraryPage = `
-    <button>button1</button>
-    <button>button2</button>    
+    <button class="button library-button">Watched</button>
+    <button class="button library-button">Queue</button>    
     <p class="visually-hidden search-result-text">
       Search result not successful. Enter the correct movie name and try
       again
@@ -47,6 +47,27 @@ const libraryGallery = `  <div class="container">
       </li>
     </ul>
   </div>`;
+const genre = [
+{id: 28, name: 'Action'},
+{id: 12, name: 'Adventure'},
+{id: 16, name: 'Animation'},
+{id: 35, name: 'Comedy'},
+{id: 80, name: 'Crime'},
+{id: 99, name: 'Documentary'},
+{id: 18, name: 'Drama'},
+{id: 10751, name: 'Family'},
+{id: 14, name: 'Fantasy'},
+{id: 36, name: 'History'},
+{id: 27, name: 'Horror'},
+{id: 10402, name: 'Music'},
+{id: 9648, name: 'Mystery'},
+{id: 10749, name: 'Romance'},
+{id: 878, name: 'Science Fiction'},
+{id: 10770, name: 'TV Movie'},
+{id: 53, name: 'Thriller'},
+{id: 10752, name: 'War'},
+{id: 37, name: 'Western'},
+]
 
 const refs = {
   homePageLinkEl: document.querySelector("a.home-link"),
@@ -57,6 +78,7 @@ const refs = {
   nav: document.querySelector("nav.nav"),
   searhFormEl: document.querySelector("form.search-form"),
   gallery: document.querySelector(".cinema-gallery"),
+  galleryListEl:document.querySelector(".cinema-gallery__list")
   
 }
 
@@ -87,5 +109,25 @@ function onHomePageLinkEl(e) {
  refs.searhFormEl.innerHTML = homePage;
 }
 
+function fetchMovie() {
+  // fetch("https://api.themoviedb.org/3/trending/all/day?api_key=c54b9b3bc824900bd0fc655039f09ff1").then(resp => resp.json()).then(console.log);
+  fetch("https://api.themoviedb.org/3/trending/all/day?api_key=c54b9b3bc824900bd0fc655039f09ff1").then(resp => resp.json()).then(resp => {
+    console.log(resp);
+    console.log(resp.results);
+    const listMovies = resp.results;
+    const gallery = listMovies.map(movie => `<li class="cinema-gallery__item">
+        <div class="thumb-img">
+          <img class="cinema-gallery__img" src=" https://image.tmdb.org/t/p/w500${movie.poster_path}">
 
+        </div>
+        <div class="thumb-text">
+          <p class="cinema-gallery__name">${movie.name || movie.title}</p>
+          <p class="cinema-gallery__text">${movie.genre_ids } | ${movie.release_date || movie.first_air_date}</p>
+        </div>`);
+    
+    refs.galleryListEl.innerHTML = gallery.join("");
 
+  })
+  }
+
+console.log(fetchMovie());
