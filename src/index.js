7923570d37b5f0939c79a5c990905922a823130e
vitaliy-1111 <networkvetalius@gmail.com>
2022-02-03 17:03:48 +0200

    add delete button

--- src/index.js
+++ src/index.js
@@ -26,6 +26,15 @@ refs.searhFormEl.addEventListener('input', debounce((onSearhFormInput), 1000));
 
 fetchMovie("movie");
 
+function fetchMovie(mediaValue) {
+  fetchMoviesByMedia(mediaValue).then(resp => {
+    console.log(resp);
+    const listMovies = resp.results;
+    renderHomeGallery(listMovies);
+    // paginationMovies();
+  })
+}
+
 function onSearhFormInput(e) {
   console.log(e.target.value);
   const searchQueryValue = e.target.value;
@@ -36,23 +45,14 @@ function onSearhFormInput(e) {
   });
 }
 
-function fetchMovie(mediaValue) {
-  const media = mediaValue;
-  fetchMoviesByMedia(media).then(resp => {
-    console.log(resp)
-    const listMovies = resp.results;
-    renderHomeGallery(listMovies);
-    // paginationMovies();
-  })
-}
-
 function onHomePageLinkEl(e) {
   e.preventDefault();
-  document.querySelector("header").classList.remove("page-header--library")
-  refs.libraryPageLinkEl.classList.remove("library-link--current")
-  refs.homePageLinkEl.classList.add("home-link--current")
+  document.querySelector("header").classList.remove("page-header--library");
+  refs.libraryPageLinkEl.classList.remove("library-link--current");
+  refs.homePageLinkEl.classList.add("home-link--current");
   refs.searhFormEl.innerHTML = homePage;
-  fetchMovie("movie")
+
+  fetchMovie("movie");
 }
 
 function onLibraryPageLinkEl(e) {
@@ -72,6 +72,7 @@ function onLibraryPageLinkEl(e) {
   document.querySelector(".library-button--watched").addEventListener('click', onLibBtnWatched)
   document.querySelector(".library-button--queue").addEventListener('click', onLibBtnQueue)
 }
+
 function onLibBtnWatched() {
   console.log("it's work, wathed");
   document.querySelector(".library-button--queue").classList.remove("library-button--active");
@@ -82,7 +83,6 @@ function onLibBtnWatched() {
     } else {
       renderLibraryGallery(localMovies, 'watched');
   }
-
 }
 
 function onLibBtnQueue() {
@@ -97,141 +97,92 @@ function onLibBtnQueue() {
     }
 }
 
+function addMovieToQueueLocalStorage(id) {
+  const localQueueList = JSON.parse(localStorage.getItem("queueList"));
+  localQueueList === null ? localQueueList : queueList = [...localQueueList];
+  // queueList = [ ...JSON.parse(localStorage.getItem("queueList"))]
+  if (!(queueList.find(item => item.id == id) && true) || false) {
+    fetchMovieById(id).then(resp => {
+      queueList.push(resp);
+      localStorage.setItem("queueList", JSON.stringify(queueList));
+    })
+  }
+}
+function addMovieToWatchedLocalStorage(id) {
+  console.log(JSON.parse(localStorage.getItem("watchedList")))
+  const localWatchedList = JSON.parse(localStorage.getItem("watchedList"));
+  localWatchedList === null ? localWatchedList : watchedList = [...localWatchedList];    
+  if (!(watchedList.find(item => item.id == id) && true) || false) {
+    fetchMovieById(id)
+    .then(resp => {
+      watchedList.push(resp);
+      localStorage.setItem("watchedList", JSON.stringify(watchedList));
+    })
+  }
+}
+
+
 document.querySelector("body").addEventListener("click", onBody);
 
 function onBody(e) {
-   e.preventDefault();
-   if (e.target.classList.contains("button-watched-delete")) {
-     let localMovies = JSON.parse(localStorage.getItem("watchedList"));
-     localMovies = localMovies.filter((movie) => movie.id != e.target.id);
-     localStorage.setItem("watchedList", JSON.stringify(localMovies));
-     localMovies = JSON.parse(localStorage.getItem("watchedList"));
-  }
-   if (e.target.classList.contains("button-queue-delete")) {
-     let localMovies = JSON.parse(localStorage.getItem("queueList"));
-     localMovies = localMovies.filter((movie) => movie.id != e.target.id);
-     localStorage.setItem("queueList", JSON.stringify(localMovies));
-     localMovies = JSON.parse(localStorage.getItem("queueList"));
-  }
-
-  // if (e.target.classList.contains("button-watched")) {
-  //   console.log(JSON.parse(localStorage.getItem("watchedList")))
-  //   const localWatchedList = JSON.parse(localStorage.getItem("watchedList"));
-  //   localWatchedList === null ? localWatchedList : watchedList = [...localWatchedList];    
-  //   if (!(watchedList.find(item => item.id == e.target.id) && true) || false) {
-  //     fetchMovieById(e.target.id)
-  //       .then(resp => {
-  //         watchedList.push(resp);      
-  //         localStorage.setItem("watchedList", JSON.stringify(watchedList));
-  //       })
-  //   }
-  //   // instance.show();
-    
-  // }  
-
-  // if (e.target.classList.contains("button-queue")) {
-  //    const localQueueList = JSON.parse(localStorage.getItem("queueList"));
-  //   localQueueList === null ? localQueueList : queueList = [...localQueueList]; 
-  //   // queueList = [ ...JSON.parse(localStorage.getItem("queueList"))]
-  //   if (!(queueList.find(item => item.id == e.target.id) && true) || false) {
-  //     fetchMovieById(e.target.id).then(resp => {
-  //       queueList.push(resp);
-  //       localStorage.setItem("queueList", JSON.stringify(queueList));
-  //     })
-  //   }
-  // }
+  e.preventDefault();
   if (e.target.classList.contains("cinema-gallery__img")) {
-    console.log('img')
     console.log(e.target.id)
     fetchMovieDetails(e.target.id).then(resp => {
-      console.log(resp);
+
       renderModalMovie(resp); 
       refs.modal.classList.toggle('is-hidden');
       refs.modal.classList.toggle('backdrop--is-hidden');
       document.querySelector('body').style.overflow = "hidden";
-      document.body.addEventListener('click', onQueueButton)
+      document.querySelector('.button-queue').addEventListener('click', onQueueButton)
+      document.querySelector('.button-watched').addEventListener('click', onWatchedButton)
+      document.querySelector('.button-queue-delete').addEventListener('click', onDeleteQueueButton)
+      document.querySelector('.button-watched-delete').addEventListener('click', onDeleteWatchedButton)
+
       function onQueueButton(event) {
         if (event.target.classList.contains('button-queue')) {
-          console.log('yes queue')
-          const localQueueList = JSON.parse(localStorage.getItem("queueList"));
-          localQueueList === null ? localQueueList : queueList = [...localQueueList];
-          // queueList = [ ...JSON.parse(localStorage.getItem("queueList"))]
-          if (!(queueList.find(item => item.id == e.target.id) && true) || false) {
-            fetchMovieById(e.target.id).then(resp => {
-              queueList.push(resp);
-              localStorage.setItem("queueList", JSON.stringify(queueList));
-            })
+             addMovieToQueueLocalStorage(e.target.id);
+           }
           }
-        }
-          if (event.target.classList.contains('button-watched')) {
-            console.log('yes wathed')
-            console.log(JSON.parse(localStorage.getItem("watchedList")))
-                const localWatchedList = JSON.parse(localStorage.getItem("watchedList"));
-                localWatchedList === null ? localWatchedList : watchedList = [...localWatchedList];    
-            if (!(watchedList.find(item => item.id == e.target.id) && true) || false) {
-              fetchMovieById(e.target.id)
-                .then(resp => {
-                  watchedList.push(resp);
-                  localStorage.setItem("watchedList", JSON.stringify(watchedList));
-                })
-            }
-            
+        
+            function onWatchedButton(event) {
+              if (event.target.classList.contains('button-watched')) {
+                addMovieToWatchedLocalStorage(e.target.id)
+              }
+      }
+      function onDeleteWatchedButton(event) {
+          let localMovies = JSON.parse(localStorage.getItem("watchedList"));
+          console.log(localMovies);
+          localMovies = localMovies.filter((movie) => movie.id != e.target.id);
+          console.log(localMovies)
+          localStorage.setItem("watchedList", JSON.stringify(localMovies));
+          localMovies = JSON.parse(localStorage.getItem("watchedList"));
+           console.log(localMovies)
+      }
 
-          }
+      function onDeleteQueueButton() {    
+       
+        let localMovies = JSON.parse(localStorage.getItem("queueList"));
+        
+        localMovies = localMovies.filter((movie) => movie.id != e.target.id);
+        
+        localStorage.setItem("queueList", JSON.stringify(localMovies));
+        
+        localMovies = JSON.parse(localStorage.getItem("queueList"));
+        
 
-        }
+      }
+
+
       
-     
-    
+
+      modalOpen(e.target.id);   
       
-  //     const refs1 = {
-  // //   openModalBtn: document.querySelector('[data-modal-open]'),
-  // //   closeModalBtn: document.querySelector('[data-modal-close]'),
-  // //   modal: document.querySelector('[data-modal]'),
-  //  queueButtonEl: document.querySelector('.button-queue'),
-  //   watchedButtonEl: document.querySelector('.button-watched')
-  //     };
-  //      let queueList = [];
-  // let watchedList = [];
-  //     refs1.queueButtonEl.addEventListener('click', onQueueButtonEl);
-  // refs1.watchedButtonEl.addEventListener('click', onWatchedButtonEl);
-
-  // function onQueueButtonEl(movie_id) {
-  //    const localQueueList = JSON.parse(localStorage.getItem("queueList"));
-  //   localQueueList === null ? localQueueList : queueList = [...localQueueList]; 
-  //   // queueList = [ ...JSON.parse(localStorage.getItem("queueList"))]
-  //   if (!(queueList.find(item => item.id == movie_id) && true) || false) {
-  //     fetchMovieById(movie_id).then(resp => {
-  //       queueList.push(resp);
-  //       localStorage.setItem("queueList", JSON.stringify(queueList));
-  //     })
-  //   }
-  // }
-  // onQueueButtonEl(e.target.id);
-
-  // function onWatchedButtonEl(movie_id) {
-  //     console.log(JSON.parse(localStorage.getItem("watchedList")))
-  //   const localWatchedList = JSON.parse(localStorage.getItem("watchedList"));
-  //   localWatchedList === null ? localWatchedList : watchedList = [...localWatchedList];    
-  //   if (!(watchedList.find(item => item.id == movie_id) && true) || false) {
-  //     fetchMovieById(movie_id)
-  //       .then(resp => {
-  //         watchedList.push(resp);      
-  //         localStorage.setItem("watchedList", JSON.stringify(watchedList));
-  //       })
-  //   }
-  // }
-  // onWatchedButtonEl(e.target.id);
-      modalOpen(e.target.id);      
-    }).then(console.log);
-    
-     // refs.modal.classList.toggle('is-hidden');
-    // refs.modal.classList.toggle('backdrop--is-hidden');
-    // document.querySelector('body').style.overflow = "hidden";
-    // modalOpen();
-
+    })
   }
-}
+  
+}  
+      
 localStorage.removeItem("watchedList")
 localStorage.removeItem("queueList")
 
--- src/js/render.js
+++ src/js/render.js
@@ -16,6 +16,7 @@ export function renderHomeGallery(listMovies,) {
         </div>`});    
   refs.galleryListEl.innerHTML = gallery.join(''); 
 }
+
 export function renderLibraryGallery(listMovies,) {
   const gallery = listMovies.map(movie => {
     return `<li class="cinema-gallery__item">
@@ -25,7 +26,7 @@ export function renderLibraryGallery(listMovies,) {
         </div>
         <div class="thumb-text">
           <p class="cinema-gallery__name">${movie.name || movie.title}</p>
-          <p class="cinema-gallery__text">${movie.genres.map(genre => genre.name)} | ${movie.release_date || movie.first_air_date || ' '}</p>
+          <p class="cinema-gallery__text">${movie.genres.map(genre => genre.name).join(' ')} | ${movie.release_date || movie.first_air_date || ' '}</p>
         </div>`});    
   refs.galleryListEl.innerHTML = gallery.join(''); 
 }
@@ -46,13 +47,17 @@ export function renderModalMovie(movie) {
           <p class="info-title">Vote / Votes<span class="info-text">${movie.vote_average} / ${movie.vote_count}</span></p>
           <p class="info-title">Popularity<span class="info-text">${movie.popularity}</span></p>
           <p class="info-title">Original Title<span class="info-text">${movie.original_title}</span></p>
-          <p class="info-title">Jenre<span class="info-text">${movie.genres.map(genre => genre.name)}</span></p>
+          <p class="info-title">Jenre<span class="info-text">${movie.genres.map(genre => genre.name).join(' ')}</span></p>
           <h3 class="modal-movie__about">About</h3>
           <p class="about-text">${movie.overview}
           </p>
           <div class="modal-movie__button-wrap">
             <button class="modal-movie__button button button-watched  id="${movie.id}">Watched</button>
             <button class="modal-movie__button button button-queue  id="${movie.id}">Queue</button>
+          </div> 
+          <div class="modal-movie__button-wrap">
+            <button class="modal-movie__button button button-watched-delete  id="${movie.id}">watched-delete</button>
+            <button class="modal-movie__button button button-queue-delete id="${movie.id}">queue-delete</button>
           </div>
         </div>`;
   refs.modalMovie.innerHTML = movieInfo;
