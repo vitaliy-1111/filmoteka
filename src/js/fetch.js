bd7cdb78e19025471c3aff1e18eef12a852ce7b0
vitaliy-1111 <networkvetalius@gmail.com>
2022-02-13 19:57:08 +0200

    refactoring fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,17 +1,21 @@
+
+const URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'c54b9b3bc824900bd0fc655039f09ff1';
+
 export function fetchMoviesByQuery(searchQueryValue, page) {
-  return fetch(`https://api.themoviedb.org/3/search/movie?api_key=c54b9b3bc824900bd0fc655039f09ff1&language=en-US&query=${searchQueryValue}&page=${page}`)
+  return fetch(`${URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchQueryValue}&page=${page}`)
     .then(resp => resp.json());
 }
 export function fetchMoviesByMedia(media, page) {
-  return fetch(`https://api.themoviedb.org/3/trending/${media}/day?api_key=c54b9b3bc824900bd0fc655039f09ff1&page=${page}`)
+  return fetch(`${URL}trending/${media}/day?api_key=${API_KEY}&page=${page}`)
     .then(resp => resp.json());
 }
 export function fetchMovieById(id) {
-  return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c54b9b3bc824900bd0fc655039f09ff1&language=en-US`)
+  return fetch(`${URL}movie/${id}?api_key=${API_KEY}&language=en-US`)
     .then(resp => resp.json());
 }
 export function fetchMovieDetails(movie_id) {
-  return fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=c54b9b3bc824900bd0fc655039f09ff1&language=en-US`)
+  return fetch(`${URL}movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
     .then(resp => resp.json());
 }
 
